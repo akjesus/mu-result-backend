e1bb89ee621dda69c505d599106f360e007d1cf2
akjesus <cutytongy@gmail.com>
2025-10-15 15:55:25 +0100

    Updated server

--- server.js
+++ server.js
@@ -4,6 +4,30 @@ const app = require("./app");
 const PORT = process.env.BACKEND_PORT;
 const IP_ADDRESS = process.env.BACKEND_SERVER_IP || "localhost";
 
-app.listen(PORT, IP_ADDRESS, () => {
-  console.log(`Backend Server is running on http://${IP_ADDRESS}:${PORT}`);
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception! Application is shutting down!');
+  process.exit(1);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(
+    `MU Result Backend is running on http://${IP_ADDRESS} at port: ${PORT}, in ${process.env.NODE_ENV} mode`
+  );
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Error! Application is shutting down!');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED! Application is shutting down!');
+  server.close(() => {
+    console.log('Process Terminated!');
+  });
 });
